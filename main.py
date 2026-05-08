2b82312909db207a802d9f25b16c11417e3cbc4a
okokchuy <aguiljes_5@icloud.com>
2026-05-07 22:39:45 -0700

    Intentionally break linting

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from dsc180-hw55!")
+import os
 
+def main():
+    unused = 42
+    print("Hello from dsc180-hw55!")
 
 if __name__ == "__main__":
     main()
+
+x = 1
+y = 2
+z = x+y
+undefined_variable
